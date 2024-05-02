5e4ac4c53c75f88e2f0421bf949bc108f4712361
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-05-02 08:21:26 +0000

    Create 0-lockboxes.py

---
+++ 0x01-lockboxes /0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""0-lockboxes.py"""
+
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+    n = len(boxes)
+    unlocked = set([0])
+    keys = boxes[0]
+    stack = list(keys)
+    while stack:
+        key = stack.pop()
+        if key < n and key not in unlocked:
+            unlocked.add(key)
+            stack.extend(boxes[key])
+    return len(unlocked) == n
