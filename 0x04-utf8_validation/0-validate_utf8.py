313825b67f4be1655c96624968ee36f6174a09b5
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-05-22 11:30:52 +0000

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,4 +6,10 @@ def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding
     """
-    pass
+    for i in data:
+        a = str(bin(i)[2:].zfill(8))
+        if int(a[0]) == 0:
+            continue
+        else:
+            return False
+    return True
