28c4dcf8f0917f2dc95235ef569843ade6e48727
MrSakine <msakine20@gmail.com>
2024-08-21 15:47:55 +0000

    function setup & docstring

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" Making Change module """
+from typing import List
+
+
+def makeChange(coins: List[int], total: int):
+    """
+    determine the fewest number of coins needed to meet a given amount total
+
+    Args:
+        - coins: list of the values of the coins in your possession
+        - total: a given amount
+
+    Return:
+        - If total is 0 or less, 0
+        - If total cannot be met by any number of coins you have, -1
+    """
+    if total == 0:
+        return 0
+
+    return -1
