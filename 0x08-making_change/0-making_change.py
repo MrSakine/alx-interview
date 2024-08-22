d35b2248a1dffac906b9459b5db0eeaf917b6e43
MrSakine <msakine20@gmail.com>
2024-08-22 09:20:53 +0000

    updated n var to float(inf)

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,28 +3,25 @@
 from typing import List
 
 
-def makeChange(coins: List[int], total: int):
+def makeChange(coins: List[int], total: int) -> int:
     """
-    determine the fewest number of coins needed to meet a given amount total
+    Determine the fewest number of coins needed to meet a given amount total.
 
     Args:
-        - coins: list of the values of the coins in your possession
-        - total: a given amount
+        - coins: List of the values of the coins in your possession.
+        - total: The total amount to achieve with the given coins.
 
     Return:
-        - If total is 0 or less, 0
-        - If total cannot be met by any number of coins you have, -1
+        - If total is 0 or less, return 0.
+        - If total cannot be met by any number of coins you have, return -1.
     """
     if total <= 0:
         return 0
-
-    n = total + 1
-    dp = [n] * (n)
+    dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for a in range(1, n):
-        for coin in coins:
-            if (a - coin) >= 0:
-                dp[a] = min(dp[a], (1 + dp[a - coin]))
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    return dp[total] if dp[total] <= total else -1
+    return dp[total] if dp[total] != float('inf') else -1
