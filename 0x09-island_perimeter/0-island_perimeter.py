fdb30ea731ebff63877a8dba8ad7f2e5cf217145
MrSakine <msakine20@gmail.com>
2024-08-27 11:34:18 +0000

    added docstring

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,4 +3,13 @@
 
 
 def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid:
+    - grid is a list of list of integers:
+        - 0 represents water
+        - 1 represents land
+        - Each cell is square, with a side length of 1
+        - Cells are connected horizontally/vertically (not diagonally).
+        - grid is rectangular, with its width and height not exceeding 100
+    """
     pass
