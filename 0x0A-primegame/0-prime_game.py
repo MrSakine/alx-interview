f938e518ec321282d2f32ea478fa4a8541d5c602
MrSakine <msakine20@gmail.com>
2024-09-05 22:02:11 +0000

    added code logic

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -14,4 +14,32 @@ def isWinner(x, nums):
         - name of the player that won the most rounds
         - If the winner cannot be determined, return None
     """
-    pass
+    def sieve_of_eratosthenes(n):
+        """ sieve algorithm """
+        sieve = [True] * (n + 1)
+        sieve[0] = sieve[1] = False
+        for i in range(2, int(n**0.5) + 1):
+            if sieve[i]:
+                for j in range(i*i, n + 1, i):
+                    sieve[j] = False
+        return sieve
+
+    max_num = max(nums)
+    primes = sieve_of_eratosthenes(max_num)
+    print(primes)
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        prime_count = sum(primes[2:n+1])
+        if prime_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
