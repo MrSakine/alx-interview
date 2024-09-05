099bf0ff101325e75bdb11720abd1207f08bfe95
MrSakine <msakine20@gmail.com>
2024-09-05 21:37:24 +0000

    added docstring

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Prime game module """
+
+
+def isWinner(x, nums):
+    """
+    Prime Game
+
+    Args:
+        - x is the number of rounds
+        - nums is an array of n
+
+    Returns:
+        - name of the player that won the most rounds
+        - If the winner cannot be determined, return None
+    """
+    pass
